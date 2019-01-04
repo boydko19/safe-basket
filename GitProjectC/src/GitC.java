c874c6f3bc8b6c28970f0a6661b143fcb182b147
Kirt Boyd <BoydKO19@pwcs-edu.org>
2019-01-04 13:27:21 -0500

    adding something

---
+++ GitProjectC/src/GitC.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class GitC {
+
+	public static void main(String[] args) 
+	{
+		System.out.println( 'Type in two integers. The first one you type must be smaller than the second one.');
+		Scanner scan = new Scanner( System.in );
+		String small = scan.next();
+		String large = scan.next();
+		scan.close();
+		System.out.println( ((Math.random() * large) * small );
+		
+	}
+
+}
