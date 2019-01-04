e36469c4a5cfaab5eb9349bfa4273be46bdcca17
Zachary Shaffer <ShaffeZP19@pwcs-edu.org>
2019-01-04 13:47:13 -0500

    kirts program except its good now

--- GitProjectC/src/GitC.java
+++ GitProjectC/src/GitC.java
@@ -4,13 +4,11 @@ public class GitC {
 
 	public static void main(String[] args) 
 	{
-		System.out.println( 'Type in two integers. The first one you type must be smaller than the second one.');
+		System.out.println( "Type in two integers. The first one you type must be smaller than the second one."); //you used single quotes KIRT
 		Scanner scan = new Scanner( System.in );
-		String small = scan.next();
-		String large = scan.next();
+		int small = Integer.parseInt(scan.next()); // u gotta take ints KIRT
+		int large = Integer.parseInt(scan.next()); // u gotta take ints KIRT
 		scan.close();
-		System.out.println( ((Math.random() * large) * small );
-		
+		System.out.println( (int)(Math.random() * (large - small + 1) ) + small); // all fixed now KIRT
 	}
-
 }
